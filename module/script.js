52f5375ce54f3bc7bdabadded4b01bc324fac8ae
AbhijithtpGit <abhijithtppc@gmail.com>
2024-08-14 22:44:00 +0530

    first commit

---
+++ module/script.js
@@ -0,0 +1,8 @@
+// module
+
+const {logger,myname} = require("./utilts");
+
+console.log(logger());
+console.log(myname);
+
+
---
+++ module/utilts.js
@@ -0,0 +1,13 @@
+
+const logger = ()=>{
+    
+    return 2*5; 
+
+};
+
+const myname = "abhijith";
+
+module.exports = {
+    logger,
+    myname,
+}
